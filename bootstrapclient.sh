0ba39a2851121a3268f92904ede0e22395e42b20
Karl Pietri <karl@threadbaregames.com>
2014-05-08 23:08:43 +0000

    fixes for new bootstrap of clients

--- bootstrapclient.sh
+++ bootstrapclient.sh
@@ -26,15 +26,21 @@ ssh ubuntu@$ADDRESS 'wget https://apt.puppetlabs.com/puppetlabs-release-precise.
 
 INSTANCE_ID=$(ssh ubuntu@$ADDRESS '/usr/bin/curl --silent http://169.254.169.254/latest/meta-data/instance-id')
 
+SERVER_INSTANCE_ID=$(/usr/bin/curl --silent http://169.254.169.254/latest/meta-data/instance-id)
+SERVER_IP=$(facter ipaddress)
+
+ssh ubuntu@$ADDRESS "sudo bash -c 'echo $SERVER_IP\ $SERVER_INSTANCE_ID >> /etc/hosts'"
+
 cd $ROOT_DIR/node_classification
 sudo ln -s $NODECONFIG.yaml $INSTANCE_ID.yaml
 sudo git add $INSTANCE_ID.yaml
 sudo git commit -m "bootstrapclient.sh adding $INSTANCE_ID as $NODECONFIG"
 sudo -E git push -u origin master
 
-ssh ubuntu@$ADDRESS "sudo puppet agent --test --waitforcert=30 --server=${LOCAL_HOSTNAME} --certname=${INSTANCE_ID}" &
+ssh ubuntu@$ADDRESS "sudo puppet agent --test --waitforcert=30 --server=${SERVER_INSTANCE_ID} --certname=${INSTANCE_ID}" &
 RUN_PID=$!
 sleep 15
 sudo -E puppet cert sign ${INSTANCE_ID}
 
 wait $RUN_PID
+
